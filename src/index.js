af39eb3a01df243148e3808d16187d545588c038
asya2320 <64478347+asya2320@users.noreply.github.com>
2022-06-26 17:23:38 +0300

    feat: update code file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  let k=0;
+  str=str.split('');
+  for(let i=0; i<str.length; i++){
+    while(bracketsConfig.length>k){
+      if(str[i]===bracketsConfig[k][1] ||str[i]===bracketsConfig[k][0]){
+        if(str[i]===bracketsConfig[k][1]&&arr[arr.length-1]===bracketsConfig[k][0]){
+          arr.pop();
+        }else{
+          arr.push(str[i]);
+        }
+      }
+      k++;
+    }
+    k=0;
+  }
+  return !arr.length;
 }
